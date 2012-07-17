f6dcf7e9290d0c191d4364f7c4d182240c4a2f5e
Daniel Lohse <daniel.lenerd.lohse@googlemail.com>
2012-07-17 16:11:07 +0200

    Refresh-Methode in Value & Node veraendert

--- Node.java
+++ Node.java
@@ -4,78 +4,88 @@ import java.util.*;
 public class Node {
 
 	private String name;			//Name der Fahrzeugkomponente
-	
+
 	private byte id;			//Zugehoerige Byte der Komponente
-	
+
 	private byte [] path;			//Pfad im Baum
-	
+
 	private Node previous;			//Zeiger auf den vorherigen Knoten
-						//nicht zwingend notwendig: unklar
+	//nicht zwingend notwendig: unklar
 	private Node nextDirectory;		//Zeiger auf Knoten der naechsten Ebene
-	
+
 	private Node sameDirectory;		//Zeiger auf Knoten der selben Ebene
-	
-	private Value values;			//Zeiger auf Liste an Values
-						//evtl auch als Hashtable möglich: unklar
-	
-        private static int counter = 1;	        //Counter Namensgebung z.B. Akuzelle10
-	
-	
+
+	private Value values = new Value();			//Zeiger auf Liste an Values
+	//evtl auch als Hashtable moeglich: unklar
+
+
 	/*+++++++++++++++Konstruktoren++++++++++++++++++++++*/
-	
+
 	//Standardkonstruktor
 	//wird in der regel nicht aufgerufen
 	public Node(){
-		this((byte)0, new byte [] {0}, "unknown");		
+		this((byte)0, new byte [] {0}, "unknown", null);		
 	}
-	
+
 	//Konstruktor Name unbekannt
 	//im Falle das der Pfad in der Hashtable fehlt
-	public Node(byte id, byte [] path){
+	public Node(byte id, byte [] path, Node previous){
+		this(id, path, "unknown_component", previous);		
+	}
+
+	//Konstruktor Name bekannt, Previous wird mituebergeben
+	public Node(byte id, byte [] path, String name, Node previous){
 		this.id = id;
 		this.path = path;
-		Node previous = this.getPrevious();
-		String name = "unknown_component_of_" + previous.name +counter;
-		
+		this.previous = previous;
+		this.setName(name);
 	}
-	
-	//Konstruktor Name bekannt
-	public Node(byte id, byte [] path, String name){
-		this.id = id;
-		this.path = path;
-	}
-	
-	
+
+
 	/*+++++++++++++++GET_METHODEN++++++++++++++++++++++*/
-	
+
 	private String getName(){
 		return this.name;
 	}
-	
+
 	private byte getID(){
 		return this.id;
 	}
-	
-	private byte [] getPath(){
-		return this.path;
+
+	public byte [] getPath(){
+		return this.path.clone();
 	}
-	
-	private Node getNextNodes(){
+
+
+	//gibt die Liste der Kindknoten zurueck
+	private Node getChildList(){
 		return this.nextDirectory;
 	}
-	
-	private Node getSameLevel(){
+
+	//gibt die Liste der Knoten mit gleichem Vaterknoten zurueck
+	private Node getNodeList(){
 		return this.sameDirectory;
 	}
-	
+
+	//gibt die naechsten freien Kindknoten zurueck
+	private Node getFreeChildNode(){
+		Node current = this.nextDirectory;
+		while(current != null){
+			current = current.sameDirectory;
+		}
+		return current;
+	}
+
+	//gibt den Vaterknoten zurueck
 	private Node getPrevious(){
 		return this.previous;
 	}
-	
+
+	//gibt die Liste der Werte zurueck
 	private Value getValues(){
 		return this.values;
 	}
-	
+
 	/*+++++++++++++++SET_METHODEN++++++++++++++++++++++*/
 
 	//setzt Namen des Attributs inkl. einer Nummerierung bei Doppelbelegung
@@ -86,13 +96,12 @@ public class Node {
 			counter++;
 		}
 		this.name = name;
-		
 	}
-	
-	//ueberprueft, ob der Name in der Directory schon verhanden ist
+
+	//ueberprueft, ob der Name in der Directory schon vorhanden ist
 	private boolean nameExist(String name){
 		Node current = previous.nextDirectory;
-		
+
 		while(current.sameDirectory != null){
 			if(name.equals(current.name)){
 				return true;
@@ -101,60 +110,67 @@ public class Node {
 		}
 		return false;
 	}
-	
-	
+
+
 	/*+++++++++++++++ADD_METHODE++++++++++++++++++++++*/
-	
-	private void add(byte [] path, byte id){	
-		
+
+	//fuegt einen Kindknoten hinzu
+	private void add(byte [] path, byte id, String name){	
+		Node current = this.getFreeChildNode();
+		current = new Node(id, path, name, this);
 	}
-	
-	
+
+
 	/*+++++++++++++++REMOVE_METHODE++++++++++++++++++++++*/
+
+	//entfernt den Knoten und alle Kondknoten (nicht notwendig)
 	private void remove(){
-		
+		this.previous = null;
 	}
-	
+
 	/*+++++++++++++++PREVIOUS_METHODEN+++++++++++++*/
-	
-	private Node getPreviousNode(){
-		return this.previous;
-	}
-	
-	private Node lastPointer(){
-		Node current = this.sameDirectory;
-		
-		while(current != null){
-			current = current.sameDirectory;
-		}
-		return current;
-	}
-	
-	
-	
+
+
+
+
+
+
 	/*+++++++++++++++SEARCH_METHODEN+++++++++++++++++*/
-	
+
+	//sucht den passenden Kindknoten anhand der ID
 	private Node getNextNode(byte id){
 		Node current = this.nextDirectory;
-		
-		 while(current != null){
-			 if(current.id == id){
-				 return current;
-			 }
-			 current = current.sameDirectory;
-		 }
-		 
-		 //throw NodeNotFoundException
-		 return current;
+
+		while(current != null){
+			if(current.id == id){
+				return current;
+			}
+			current = current.sameDirectory;
+		}
+
+		//throw NodeNotFoundException
+		return current;
 	}
-	
-	
+
+
 	/*+++++++++++++++REFRESH_METHODEN+++++++++++++++++*/
-	
-	//saemtliche Values des aktuellen und der UnterKnoten werden rekursiv aktuallisiert
+
+	//TODO: saemtliche Values des aktuellen und der UnterKnoten werden rekursiv aktuallisiert
+	//Das Feld mit pfad und Befehl wird zurückgegeben
 	private byte [] refresh(){
 		return null;
 	}
-	
+
+	private byte [] refreshValues(){
+		Value current = this.values;
+		
+		while(current != null){
+			byte [] commandField = current.refresh();
+
+		}
+		
+		return null;
+
+	}
 
 }
--- Value.java
+++ Value.java
@@ -2,61 +2,110 @@ package com.example.communicationservice;
 import java.util.*;
 
 public class Value{
-	
+
 	byte id;
 	String name;
 	Date zeitstempel;
 	Object value;
 	Value next;
 	Node vather;
-	
-	
+
+	public Value(){
+
+	}
 	public Value(byte id, String name, Object value){
 		this.id = id;
 		this.name = name;
 		this.value = value;
 	}
-	
+
 	public Value(byte id, String name, Object value, Date zeitstempel){
 		this(id, name, value);
 		this.zeitstempel = zeitstempel;
-		
-	}
-	
-	
-	public Value searchValue(byte id){
-		return null;
-	}
-	
-	public Value lastPointer(Value values){
-		return null;
-	}
-	
-	public void refresh(){
-		
+
 	}
 
-	public Value getLastValue() {
+	public byte getID(){
+		return this.id;
+	}
+
+	private Value searchValue(byte id){
+		return null;
+	}
+
+	private Value lastPointer(Value values){
+		return null;
+	}
+
+	public byte [] refresh(){
+		
+		byte [] path = vather.getPath();
+		byte depth = (byte) path.length;
+		byte command = (byte) 2; //muss noch ermittelt werden
+		byte length = (byte) (depth + (byte) 4);
+		byte [] commandSet = new byte [length];
+		
+		for(int i = 0; i < commandSet.length; i++){
+			switch(i){
+			case 0: 
+				commandSet[i] = length;
+			case 1: 
+				commandSet[i] = command;
+			case 2: 
+				commandSet[i] = depth;
+			default: 
+				if(i != length-1){
+					commandSet[i] = path[i- 3];
+				}else{
+					commandSet[i] = this.id;
+				}
+				
+			}
+		}
+		return commandSet.clone();
+	}
+
+	private Value getLastValue() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public void addValue(byte type, byte[] path, byte id) {
-		
+	private void add(byte type, byte[] path, byte id) {
+
 		switch(type){
 		}
 	}
-	private void addByte(byte id, String name) {
-		this.next = new Value(id, name, new Byte(null));
-		
-	}
-	private void addInteger(byte id, String name) {
-		this.next = new Value(id, name, new Integer(null));
-	}
+	
 	private void addBoolean(byte id, String name) {
 		this.next = new Value(id, name, new Boolean(null));
-		
+
+	}
+	private void addByte(byte id, String name) {
+		this.next = new Value(id, name, new Byte(null));
 	}
 	
+	private void addChar(byte id, String name){
+		char value = 0;
+		this.next = new Value(id, name, value);
+	}
 	
+	private void addDouble(byte id, String name){
+		this.next = new Value(id, name, new Double(null));
+	}
+	
+	private void addFloat(byte id, String name){
+		this.next = new Value(id, name, new Float(null));
+	}
+	
+	private void addInteger(byte id, String name){
+		this.next = new Value(id, name, new Integer(null));
+	}
+
+	private void addLong(byte id, String name){
+		this.next = new Value(id, name, new Long(null));
+	}
+	private void addShort(byte id, String name){
+		this.next = new Value(id, name, new Short(null));
+	}
+
 }
